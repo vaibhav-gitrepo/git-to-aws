6f4dd61cf3102ae080269d591ee63bbb99de4a10
Vaibhav D <vaibhavsdevadkar@gmail.com>
2025-10-24 18:49:45 +0530

    initial commit:Flask API with RDS Mysql Integration

---
+++ app.py
@@ -0,0 +1,36 @@
+from flask import Flask, render_template, request, redirect
+import mysql.connector
+
+app = Flask(__name__)
+
+db = mysql.connector.connect(
+    host="database-1.czi4uwuyaevt.eu-north-1.rds.amazonaws.com",
+    user="admin",
+    password="abcd1234",
+    database="Mydatabase"
+)
+cursor = db.cursor()
+
+@app.route('/')
+def index():
+    cursor.execute("SELECT * FROM contacts")
+    users = cursor.fetchall()
+    return render_template('index.html', users=users)
+
+@app.route('/add', methods=['POST'])
+def add():
+    name = request.form['name']
+    email = request.form['email']
+    phone = request.form['phone']
+    cursor.execute("INSERT INTO contacts (name, email, phone) VALUES (%s, %s, %s)", (name, email, phone))
+    db.commit()
+    return redirect('/')
+
+@app.route('/delete/<int:id>', methods=['POST'])
+def delete(id):
+    cursor.execute("DELETE FROM contacts WHERE id = %s", (id,))
+    db.commit()
+    return redirect('/')
+
+if __name__ == '__main__':
+    app.run(debug=True)
